3fa3c693da4f2d40235bec11e52b319c50bbbe17
ZkClown <contact@kphamle.com>
2017-08-10 21:57:49 +0200

    Add LoadCsv CSV

--- genPwd.py
+++ genPwd.py
@@ -1,15 +1,29 @@
 import csv
 from threading import Thread
 
+def loadCsv(file):
+    res = []
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=";")
+        for i in reader:
+            res.append(i)
 
+    return res
 
 class word:
     def __init__(self, word):
         self.word = word
         self.done = []
 
-    def genWords():
+    def genWords(dictionary):
         res = ""
+
+
+
+
+
+
+
         if self.checkExist(res) != 0:
             self.genWords()
         else:
